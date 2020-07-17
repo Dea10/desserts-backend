2552efd1f91ac12f2b1e6757c14c0564236b0331
Daniel Espinosa <d111092.de@gmail.com>
2020-07-16 21:42:10 -0500

    MONGO_URL forced

--- server/config/config.js
+++ server/config/config.js
@@ -4,14 +4,14 @@ process.env.PORT = process.env.PORT || 8080;
 
 // --- Environment ---
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+// process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
-// --- Mongo DB ---
+// // --- Mongo DB ---
 
-if(process.env.NODE_ENV === 'dev') {
-    DBURL = 'mongodb://localhost:27017/desserts';
-} else {
-    DBURL = process.env.MONGO_URL
-}
+// if(process.env.NODE_ENV === 'dev') {
+//     DBURL = 'mongodb://localhost:27017/desserts';
+// } else {
+//     DBURL = process.env.MONGO_URL
+// }
 
-process.env.DBURL = DBURL
+process.env.DBURL =  process.env.MONGO_URL
